8665426fc06516342320761023bdbb657e565018
Dominick Peluso <peluso.dominick@gmail.com>
2014-11-07 14:51:30 -0500

    Absolute links to images

--- src/Shawmut/VeracoreApi/Response.php
+++ src/Shawmut/VeracoreApi/Response.php
@@ -2,10 +2,47 @@
 
 namespace Shawmut\VeracoreApi;
 
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Yaml\Exception\ParseException;
+
 class Response
 {
+
+    protected function getSettings()
+    {
+        $yaml = new Parser();
+
+        try {
+            $value = $yaml->parse(file_get_contents(__DIR__.'/../../../app/settings.yml'));
+        } catch (ParseException $e) {
+            printf("Unable to parse the YAML string: %s", $e->getMessage());
+        }
+
+        return $value;
+    }
+
+    protected function setAbsoluteLinks($getOffers)
+    {
+        $settings =  $this->getSettings();
+
+        #print_r($getOffers); die;
+
+        foreach($getOffers->GetOffersResult->GetOfferResult as &$getOfferResult)
+        {
+            $getOfferResult->ImagePath = $settings['veracore_api']['base_path'].$getOfferResult->ImagePath;
+            $getOfferResult->FullImagePath = $settings['veracore_api']['base_path'].$getOfferResult->FullImagePath;
+        }
+
+        return $getOffers->GetOffersResult->GetOfferResult;
+    }
+
     public function getResponseSuccess($result)
     {
+
+        // Send out to fix links if GetOffersResult
+        if($result->GetOffersResult) $result = $this->setAbsoluteLinks($result);
+
+        // Otherwise return the result as is
         $response = new \stdClass();
         $response->type = "Success";
         $response->body = $result;
